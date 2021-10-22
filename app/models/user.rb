f79e11f5354a1b63395760291621c3321f16e2f7
t.miura <t.miura@cybermissions@co.jp>
2021-10-22 16:15:20 +0900

    ユーザー登録のvalidation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name , length: { minimum: 2 , maximum: 20, message: "は2文字以上20文字以下で設定してください"}
+  validates :age, length: { minimum: 18, message: "17歳以下の方のご利用は出来ません" }
+  validates :password, presence:  true , length: { minimum: 6, message:"パスワードは６文字以上で設定してください" }
   has_many :votes, dependent: :destroy        
 end
